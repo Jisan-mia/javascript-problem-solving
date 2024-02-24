ce3d50f2edda4a3100b7da4e0caa62dff2a8d515
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-02-25 01:50:03 +0600

    flatten array with reduce() and recursion

--- on-site-coding-problems/flatten-arr.js
+++ on-site-coding-problems/flatten-arr.js
@@ -4,24 +4,43 @@
 // flatten(exampleArray); // [1,2,3,4,5,6,7,8,9,10]
 
 function flatten(arr) {
-  const flattenedArr = []
-  for(let i = 0; i < arr.length; i++) {
-    const element = arr[i]
-    console.log(element)
-    if(Array.isArray(element)) {
-      flattenedArr.push(...flatten(element))
+  const flattenedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+    if (Array.isArray(element)) {
+      flattenedArr.push(...flatten(element));
     } else {
-      flattenedArr.push(element)
+      flattenedArr.push(element);
     }
   }
   return flattenedArr;
 }
 
-const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
+const exampleArray = [1, 2, [3, 4, [5, 6, 7], 8], 9, 10];
 console.log(flatten(exampleArray)); // [1,2,3,4,5,6,7,8,9,10]
 
-
+// solution with reduce() and recursion
 function flatten2(arr) {
-  return arr.reduce((acc, currentElement) => acc.concat(Array.isArray(currentElement) ? flatten2(currentElement) : currentElement), [])
+  arr.reduce((acc, currentElement) => {
+    if(Array.isArray(currentElement)) {
+      acc.concat(flatten2(currentElement))
+    } else {
+      acc.push(currentElement)
+    }
+    return acc
+  }, [])
 }
+
+// function flatten2(arr) {
+//   return arr.reduce(
+//     (acc, currentElement) =>
+//       acc.concat(
+//         Array.isArray(currentElement)
+//           ? flatten2(currentElement)
+//           : currentElement
+//       ),
+//     []
+//   );
+// }
 console.log(flatten2(exampleArray));
